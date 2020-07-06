33c45a062e7cd0b7db7c4345894fd551d0104167
thedocbwarren <jedi.master@augmentedjs.com>
2020-07-06 11:51:27 -0700

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import ViewController from "./viewController.js";
 import ManagedController from "./managedController.js";
-
-module.exports.ViewController = ViewController;
-module.exports.ManagedController = ManagedController;
+export { ViewController, ManagedController };
